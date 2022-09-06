08c405b35f97930d5062e40f71d71162e98c68fb
Bruno Reck Gambim <54690203+BrunoGambim@users.noreply.github.com>
2022-09-05 21:28:37 -0300

    Delete member_data.h

--- member_data.h
+++
@@ -1,9 +0,0 @@
-typedef struct _member_data{
-    char *hostname;
-    char *macAddress;
-    char *ipAddress;
-    char *status;
-} member_data;
-
-void createMemberData(char *hostname, char *macAddress, char *ipAddress, char *status, member_data** data);
-void freeMemberData(member_data* data);
